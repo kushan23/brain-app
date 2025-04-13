1df969089ca586fb42ae649865c3541398a49695
Kushan Arora <kushan@192.168.1.7>
2025-04-13 19:15:42 +0530

    ;;;

--- brain-frontend/brain-front/vite.config.ts
+++ brain-frontend/brain-front/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    allowedHosts: ['brain.kushan23.com', 'localhost', '2401:4900:1c17:b411:89c9:c504:9ea6:3eb3%']
+  },
   base: "/",
   plugins: [react()],
+  
 })
